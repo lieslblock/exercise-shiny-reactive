97ba275aabbef8e30cd30f6f8de48fde01054551
Liesl Block <lblock@uw.edu>
2019-05-28 16:00:01 -0700

    assignment edits

--- app.R
+++ app.R
@@ -19,6 +19,19 @@ ui <- fluidPage(
     sidebarPanel(
       # Put a widget here for the user to interact with!
       # You could filter by year, by county, etc.
+      selectInput("chosenCounties", 
+                  label = "Counties to view:",
+                  choices = unique(data$CountyName),
+                  selected = unique(data$CountyName),
+                  multiple = TRUE),
+    
+    sliderInput("chosenYears",
+                "Year",
+                min = min(data$ReportYear),
+                max = max(data$ReportYear),
+                value = c(min(data$ReportYear), max(data$ReportYear)),
+                sep = "",
+                step = 1)
     ),
     
     mainPanel(
@@ -32,15 +45,28 @@ ui <- fluidPage(
 
 # Server
 server <- function(input, output) {
+  chosen_data <- reactive({
+    data %>%
+      filter(CountyName %in% input$chosenCounties,
+             ReportYear >= input$chosenYears[1],
+             ReportYear <= input$chosenYears[2])
+  })
+  
   # A plot of air quality
   # This can be over time, by county, etc.
   output$airQualityPlot <- renderPlot({
-    
+    chosen_data() %>% 
+      ggplot() +
+      geom_line(aes(x = ReportYear, y = Value, color = CountyName)) +
+      labs(title = "Washington state air quality over time",
+           subtitle = "Percent of days with PM2.5 levels over the National Ambient Air Quality Standard (NAAQS)",
+           x = "Year",
+           y = "Percent of unsafe days")
   })
   
   # A table of the filtered data
   output$airQualityTable <- renderTable({
-    
+    chosen_data()
   })
 }
 
